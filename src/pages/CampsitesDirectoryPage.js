79f3b4195785b83707b6cdeface8838a8ab73efb
veganpowerlifter <katya@totalbodylab.com>
2023-07-07 16:43:32 -0400

    usestat hook

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,15 +1,19 @@
-import { Container, Row, Col, Button } from 'reactstrap';
+import {useState} from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 
-const CampsitesDirectoryPage = () => {
-    const selectedCampsite = selectRandomCampsite();
+const CampsiteDirectoryPage = () =>{
+    const [campsiteId, setCampsiteId] = useState(0);
+    const selectedCampsite = selectCampsiteById(campsiteId);
+
     return (
         <Container>
+
             <Row>
                 <Col sm='5' md='7'>
-                    <CampsitesList />
+                <CampsitesList setCampsiteId={setCampsiteId} />
                 </Col>
                 <Col sm='7' md='5'>
                     <CampsiteDetail campsite={selectedCampsite} />
@@ -19,4 +23,4 @@ const CampsitesDirectoryPage = () => {
     );
 };
 
-export default CampsitesDirectoryPage;
+export default CampsiteDirectoryPage;
