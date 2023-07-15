d1f403f94725e96cb7e0f965df9ab3b03d59be07
veganpowerlifter <katya@totalbodylab.com>
2023-07-15 16:13:50 -0400

    week 3 task 2 completed

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,20 +1,87 @@
 import useState from 'react';
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { Formik, Field, Form } from 'formik';
 
 const CommentForm = ({ campsiteid }) => {
 
     const [modalOpen, setModalOpen] = useState(false);
+
+    const handleSubmit = (values) => {
+        const comment = {
+            campsiteId: parseInt(campsiteId),
+            rating: values.rating,
+            author: values.author,
+            text: values.commentText
+        };
+        console.log(comment);
+        setModalOpen(false);
+    };
+
     return (
         <> {/* react fragment wraps around the button and other elements*/}
             <Button onClick={() => setModalOpen(true)}>   {/* we pass the arrow function as a call back function*/}
                 <i className='fa fa-pencil fa-lg' /> Add Comment
             </Button>
             <Modal isOpen={modalOpen}>
-                <ModalHeader toggle={()=> setModalOpen(false)}>
+                <ModalHeader toggle={() => setModalOpen(false)}>
                     Add Comment
                 </ModalHeader>
-               < ModalBody> campsite:{campsiteID}</ModalBody>
-            </Modal>
+                < ModalBody>
+                    <Formik initialValue={{
+                        rating,
+                        author,
+                        commentText: ''
+                    }}
+                        onSubmit={handleSubmit}
+                    >
+                        <Form>
+                            <FormGroup>
+                                <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                                <Label htmlFor={:'rating'} >
+                                Rating
+                            </Label>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Field
+                                name='author'
+                                placeholder='Your Name'
+                                className='form-control'
+                            />
+                            <Label htmlFor={:'author'} >
+                            Your Name
+                        </Label>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Field
+                            name='commentText'
+                            as='textarea'
+                            rows='12'
+                            className='form-control'
+                        />
+                        <Label htmlFor={:'commentText'} >
+                        Comment
+                    </Label>
+                </FormGroup>
+                <Button type='submit' color='primary'>
+                    Submit
+                </Button>
+            </Form>
+        </Formik >
+               </ModalBody >
+            </Modal >
         </>
     )
 };
---
+++ src/components/CommentsLists.js
@@ -0,0 +1,8 @@
+import CommentForm from '../components.CommentForm.js';
+
+Comment={()=> comments.map()};
+
+const CommentForm = () => {
+    const campsiteId = (commentform, { resetForm }) => {
+        console.log('form values:', values);
+    };};
